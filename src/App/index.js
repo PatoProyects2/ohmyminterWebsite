c116631fd2fa9d01c4e6237ec12b2a221f5807a2
PatoVerde <leandrodenos@gmail.com>
2022-06-10 09:25:48 -0300

    all dependencies, lets go to work

---
+++ src/App/index.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './App.css';
+import MainMint from './MainMint';
+import NavBar from './NavBar';
+
+function App() {
+  const [account, setAccount] = useState([]);
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
